3e963535c821f4873f040cebd294dbdb64e9155d
Mike <mjeanjacques90@gmail.com>
2019-04-27 22:26:02 -0400

    styling, alert score, media queries

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,60 @@
 $(document).ready(function() {
 
-    var counter = 0;
-    var correctAnswer = 0;
-    var userChoice;
-    var number = 5;
-    var intervalId;
+  var counter = 0;
+  var correctAnswer = 0;
+  var userChoice;
+  // var number = 10;
+  var intervalId;
 
 
-var questionsObj = [
-    {question:"On average, how many different parts does a standard car have?", answer:"d"},
-    {question:"During a car accident, how fast do airbags inflate?",answer:"b"},
-    {question:"What was the first automobile with an internal combustion engine?",answer:"a"},
-    {question:"How many cars are produced worldwide every year?",answer:"b"},
-    {question:"What percentage of of electric/hybrid vehicles are sold in Norway each year?",answer:"a"},
-    
-]
+  var questionsObj = [
+      {question:"On average, how many different parts does a standard car have?", answer:"d"},
+      {question:"During a car accident, how fast do airbags inflate?",answer:"b"},
+      {question:"What was the first automobile with an internal combustion engine?",answer:"a"},
+      {question:"How many cars are produced worldwide every year?",answer:"b"},
+      {question:"What percentage of of electric/hybrid vehicles are sold in Norway each year?",answer:"a"},
+      
+  ]
 
-var answersObj = [
+  var answersObj = [
     {choiceA:"850", choiceB:"5,000", choiceC:"1,500", choiceD:"30,000"},
     {choiceA:"1 Second", choiceB:"30 Milliseconds", choiceC:"5 Seconds", choiceD:"100 Milliseconds"},
     {choiceA:"Benz Motorwagon", choiceB:"Ford Model T", choiceC:"Studebaker Flanders 20", choiceD:"Maybach Stahlradwagen"},
     {choiceA:"410,000", choiceB:"165,000", choiceC:"55,000", choiceD:"1 Million"},
     {choiceA:"50%", choiceB:"10%", choiceC:"75%", choiceD:"30%"}
-]
+  ]
+  
+  function stop() {
+    clearInterval(intervalId);
+    clockRunning = false;
+    
+  }
 
-function clearText(){
+  function clearText(){
     $("#target").empty();
     $("#question").empty();
-}
+  }
 
-function populateQuestions(){
+  function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  function decrement(){
+    number--;
+    $("#timer").text(number);
+
+    if (number === 0) {
+      stop();
+      $('#submitBtn').trigger('click');
+    }
+  }
+
+  function stop() {
+    clearInterval(intervalId);
+  }
+
+  function populateQuestions(){
     var radioAnswerA = $("<p id='answerA'><input type='radio' name='q-"+counter+"' value=a>a. "+answersObj[counter].choiceA+"</p>")
     var radioAnswerB = $("<p id='answerB'><input type='radio' name='q-"+counter+"' value=b>b. "+answersObj[counter].choiceB+"</p>")
     var radioAnswerC = $("<p id='answerC'><input type='radio' name='q-"+counter+"' value=c>c. "+answersObj[counter].choiceC+"</p>")
@@ -44,29 +69,17 @@ function populateQuestions(){
     radioAnswerD.appendTo("#target");
     console.log('question # '+counter)
 
-    number = 5;
-    
-}
+    number = 15;
+  }
 
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
+  function lastQuestion(){
+    if(counter == questionsObj.length-1){
+      var element = document.getElementById("submitBtn");
+      element.parentNode.removeChild(element);
+      alert(correctAnswer+"/"+questionsObj.length);
     }
+  }
 
-    function decrement(){
-      number--;
-      $("#timer").text(number);
-
-      if (number === 0) {
-        stop();
-        $('#submitBtn').trigger('click');
-        alert(userChoice);
-      }
-    }
-
-    function stop() {
-      clearInterval(intervalId);
-    }
 
     
     for(i=0; i < questionsObj.length; i++){
@@ -77,21 +90,19 @@ function populateQuestions(){
                 userChoice = $("input[name='q-"+counter+"']:checked").val();
                 console.log(userChoice);
                 if(userChoice==questionsObj[counter].answer){
-                    // alert('correct');
+                    console.log('correct');
                     correctAnswer++;
+                  lastQuestion();
+                }else{
+                  console.log("wrong");
+                  lastQuestion();
                 }
                 clearText();
                 counter++;
                 populateQuestions();
                 run();
             })            
-        }
-        if (i > questionsObj.length){
-            break;
-        }
-        
+        }      
     }
-    // alert(correctAnswer+"/"+questionsObj.length)
-
-    // alert('This should pop up second')
+    
 })
