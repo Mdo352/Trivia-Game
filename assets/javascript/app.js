70e3e1e5ff7e8822760afac867348d0008837c49
Mike <mjeanjacques90@gmail.com>
2019-04-29 12:39:10 -0400

    break main for loop and show final score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   var userChoice;
   // var number = 10;
   var intervalId;
+  var numberOfQuestions = 5;
+  var questionsLeft = true;
 
 
   var questionsObj = [
@@ -13,6 +15,7 @@ $(document).ready(function() {
       {question:"What was the first automobile with an internal combustion engine?",answer:"a"},
       {question:"How many cars are produced worldwide every year?",answer:"b"},
       {question:"What percentage of of electric/hybrid vehicles are sold in Norway each year?",answer:"a"},
+      // {question:"You got "+numberOfQuestions+" out of "+5+" ,answers correct", answer:""},
       
   ]
 
@@ -21,7 +24,8 @@ $(document).ready(function() {
     {choiceA:"1 Second", choiceB:"30 Milliseconds", choiceC:"5 Seconds", choiceD:"100 Milliseconds"},
     {choiceA:"Benz Motorwagon", choiceB:"Ford Model T", choiceC:"Studebaker Flanders 20", choiceD:"Maybach Stahlradwagen"},
     {choiceA:"410,000", choiceB:"165,000", choiceC:"55,000", choiceD:"1 Million"},
-    {choiceA:"50%", choiceB:"10%", choiceC:"75%", choiceD:"30%"}
+    {choiceA:"50%", choiceB:"10%", choiceC:"75%", choiceD:"30%"},
+    // {choiceA:"", choiceB:"", choiceC:"", choiceD:""}
   ]
   
   function stop() {
@@ -53,36 +57,41 @@ $(document).ready(function() {
   function stop() {
     clearInterval(intervalId);
   }
-
+  
   function populateQuestions(){
-    var radioAnswerA = $("<p id='answerA'><input type='radio' name='q-"+counter+"' value=a>a. "+answersObj[counter].choiceA+"</p>")
-    var radioAnswerB = $("<p id='answerB'><input type='radio' name='q-"+counter+"' value=b>b. "+answersObj[counter].choiceB+"</p>")
-    var radioAnswerC = $("<p id='answerC'><input type='radio' name='q-"+counter+"' value=c>c. "+answersObj[counter].choiceC+"</p>")
-    var radioAnswerD = $("<p id='answerD'><input type='radio' name='q-"+counter+"' value=d>d. "+answersObj[counter].choiceD+"</p>")
-    var questionCycle = $("<p>"+questionsObj[counter].question+"</p>")
-
-    $("#question").text(questionsObj[counter].question);
-
-    radioAnswerA.appendTo("#target");
-    radioAnswerB.appendTo("#target");
-    radioAnswerC.appendTo("#target");
-    radioAnswerD.appendTo("#target");
-    console.log('question # '+counter)
-
-    number = 15;
+    if (questionsLeft == true){
+      var radioAnswerA = $("<p id='answerA'><input type='radio' name='q-"+counter+"' value=a>a. "+answersObj[counter].choiceA+"</p>")
+      var radioAnswerB = $("<p id='answerB'><input type='radio' name='q-"+counter+"' value=b>b. "+answersObj[counter].choiceB+"</p>")
+      var radioAnswerC = $("<p id='answerC'><input type='radio' name='q-"+counter+"' value=c>c. "+answersObj[counter].choiceC+"</p>")
+      var radioAnswerD = $("<p id='answerD'><input type='radio' name='q-"+counter+"' value=d>d. "+answersObj[counter].choiceD+"</p>")
+      var questionCycle = $("<p>"+questionsObj[counter].question+"</p>")
+      
+      $("#question").text(questionsObj[counter].question);
+      
+      radioAnswerA.appendTo("#target");
+      radioAnswerB.appendTo("#target");
+      radioAnswerC.appendTo("#target");
+      radioAnswerD.appendTo("#target");
+      console.log('question # '+counter)
+      
+      number = 15;
+    }
   }
-
+  
   function lastQuestion(){
     if(counter == questionsObj.length-1){
       var element = document.getElementById("submitBtn");
       element.parentNode.removeChild(element);
-      alert(correctAnswer+"/"+questionsObj.length);
+      // alert(correctAnswer+"/"+questionsObj.length);
+      $("#question").append("final score will go here");
+      console.log(correctAnswer)
     }
   }
-
-
+  
+  
     
     for(i=0; i < questionsObj.length; i++){
+      if(questionsLeft == true){
         if (i == counter){
             populateQuestions();
             run();
@@ -90,8 +99,8 @@ $(document).ready(function() {
                 userChoice = $("input[name='q-"+counter+"']:checked").val();
                 console.log(userChoice);
                 if(userChoice==questionsObj[counter].answer){
-                    console.log('correct');
-                    correctAnswer++;
+                  console.log('correct');
+                  correctAnswer++;
                   lastQuestion();
                 }else{
                   console.log("wrong");
@@ -99,10 +108,23 @@ $(document).ready(function() {
                 }
                 clearText();
                 counter++;
+                if(counter >= questionsObj.length){
+                  questionsLeft = false;
+                }
                 populateQuestions();
                 run();
-            })            
-        }      
+                if (questionsLeft == false){
+                  var finalScore = correctAnswer/questionsObj.length;
+                  console.log("Final Score shoul pop up");
+                  $("#question").text("You got "+correctAnswer+" questions correct");
+                  $("#target").text(finalScore * 100 + '%');
+                }           
+            })
+        }
+
+      }else{
+      }
+      
     }
     
 })
